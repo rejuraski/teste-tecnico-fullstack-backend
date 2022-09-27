9fbafedbb2327e9de2ca3cc7271c689d96ade6c9
Renata <re_juraski@hotmail.com>
2022-09-26 22:16:53 -0300

    fix/command

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,31 +1,35 @@
+import "dotenv/config";
+import "reflect-metadata";
 import { DataSource } from "typeorm";
-require("dotenv").config();
 
-export const AppDataSource =
-  process.env.NODE_ENV === "production"
-    ? new DataSource({
-        type: "sqlite",
-        database: ":memory:",
-        entities: ["src/entities/*.ts"],
-        synchronize: true,
-      })
-    : new DataSource({
-        type: "postgres",
-        host: "localhost",
-        port: 5432,
-        username: "renata",
-        password: "sucesso",
-        database: "juraski_db",
-        synchronize: false,
-        logging: true,
-        entities: ["src/entities/*.ts"],
-        migrations: ["src/migrations/*.ts"],
-      });
+const host = process.env.IS_COMPOSE ? "db" : "localhost";
 
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Data Source initialized");
-  })
-  .catch((err) => {
-    console.error("Error during Data Source initialization", err);
-  });
+const synchronize = process.env.NODE_ENV === "test" ? true : false;
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  // host,
+  // port: 5432,
+  // username: process.env.POSTGRES_USER,
+  // password: process.env.POSTGRES_PASSWORD,
+  // database: process.env.POSTGRES_DB,
+  // synchronize: false,
+  // logging: false,
+  // entities: ["./src/entities/*.ts"],
+  // migrations: ["./src/migrations/*.ts"],
+  url: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+  synchronize,
+  logging: false,
+  entities:
+    process.env.NODE_ENV === "production"
+      ? ["./dist/src/entities/*.js"]
+      : ["./src/entities/*.ts"],
+  migrations:
+    process.env.NODE_ENV === "production"
+      ? ["./dist/src/migrations/*.js"]
+      : ["./src/migrations/*.ts"],
+});
